24801ee7e35fe0aa3dfd765d2b28e6f327109cd9
Özgür Isbert <oezguerisbert@gmail.com>
2024-01-23 10:35:57 +0100

    fix: server adapter

--- packages/app/astro.config.mjs
+++ packages/app/astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from "@astrojs/mdx";
 import solid from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import aws from "astro-sst";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://oezguerisbert.com",
   integrations: [mdx(), sitemap(), solid(), tailwind()],
-  output: "hybrid",
+  output: "server",
+  adapter: aws(),
 });
