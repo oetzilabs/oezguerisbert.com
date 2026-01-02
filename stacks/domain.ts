365e873d175065ca95495844f71e2e8581a22520
Özgür Isbert <oezguerisbert@gmail.com>
2026-01-02 02:27:00 +0100

    feat: update

---
+++ stacks/domain.ts
@@ -0,0 +1,7 @@
+export const domain = (() => {
+  if ($app.stage === "production") return "oezguerisbert.com"
+  if ($app.stage === "dev") return "dev.oezguerisbert.com"
+  return `${$app.stage}.dev.oezguerisbert.com`
+})()
+
+export const zoneID = "317bb796e16c10365fc142e7edb43b0e"
